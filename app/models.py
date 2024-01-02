6ee572dcc0fa7ab5f86e3b5f77b213a12ada8aee
VictorAlexandreBragaRibeiro1986 <victoralexandre29051986@gmail.com>
2024-01-02 13:46:49 -0300

    Feito o model

--- app/models.py
+++ app/models.py
@@ -9,3 +9,13 @@ class Cliente(db.Model):
     senha = db.Column(db.String(20), nullable=False)
     cpf = db.Column(db.String(11), unique=True, nullable=False)
     
+class Veiculos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    placa = db.Column(db.String(10), nullable=False, unique=True)
+    ano = db.Column(db.String(4), nullable=False)
+    qtd_portas = db.Column(db.Integer, nullable=False)
+    
+class Eletronico(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    voltagem = db.Column(db.String(3), nullable=False)
+    
