5910fb64958701b888ffd0b8971ebd2d8011b325
VictorAlexandreBragaRibeiro1986 <victoralexandre29051986@gmail.com>
2024-01-02 13:51:10 -0300

    Feito o schema de veiculo e eletronico

--- app/schemas.py
+++ app/schemas.py
@@ -7,3 +7,15 @@ class ClienteSchema(Schema):
     senha = fields.Str(required=True, load_only=True)
     telefone = fields.Str(required=True)
     cpf = fields.Str(required=True)
+    
+    
+class VeiculoSchema(Schema):
+    id = fields.Int(dump_only=True)
+    placa = fields.Str(required=True)
+    ano = fields.Str(required=True)
+    qtd_portas = fields.Str(required=True)
+    
+
+class EletronicoSchema(Schema):
+    id = fields.Int(dump_only=True)
+    
