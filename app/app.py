98f6d5d4c7e99333af8b39ee76767cbfd1fd3db8
MatheusSakuragui <matheus.lemesakuragui@gmail.com>
2024-01-03 10:54:41 -0300

    [fix] corrigido importacap

--- app/app.py
+++ app/app.py
@@ -4,9 +4,11 @@ from flask import Flask
 from app.config import Config
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
-from app.resources.cliente import ClienteResource
+from app.resources.produto import ProdutoResource
+from app.resources.financeiro import FinanceiroResource
+from app.resources.conta import ContaResource
+from app.resources.cliente import ClienteResource, ClienteLogin
 from app.resources.leilao import LeilaoResource, LeilaoResourceLista
-
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
@@ -16,6 +18,10 @@ def create_app():
     
     api = Api(app)
     api.add_resource(ClienteResource, '/clientes', '/clientes/<int:id>')
+    api.add_resource(ProdutoResource, '/produtos', '/produtos/<int:id>')
+    api.add_resource(FinanceiroResource, '/financeiro', '/financeiro/<int:id>')
+    api.add_resource(ContaResource, '/conta', '/conta/<int:id>')
+    api.add_resource(ClienteLogin, '/login')
     api.add_resource(LeilaoResource, '/leilao', '/leilao/<int:id>')
     api.add_resource(LeilaoResourceLista, '/listaleilao')
     
