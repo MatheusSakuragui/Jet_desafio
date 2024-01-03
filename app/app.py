34b4c49aa7a9abcee87a56d92005fc73970189ef
MatheusSakuragui <matheus.lemesakuragui@gmail.com>
2024-01-03 13:09:41 -0300

    [fix] import app.py fix

--- app/app.py
+++ app/app.py
@@ -4,13 +4,12 @@ from flask import Flask
 from app.config import Config
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
-from app.resources.produto import ProdutoResource, ProdutoResourceLista
+from app.resources.produto import ProdutoResource
 from app.resources.financeiro import FinanceiroResource
 from app.resources.conta import ContaResource
 from app.resources.cliente import ClienteResource, ClienteLogin
-from app.resources.lance import LanceResource
 from app.resources.leilao import LeilaoResource, LeilaoResourceLista
-from app.scheduler import schedule
+from app.scheduler import scheduler
 
 def create_app():
     app = Flask(__name__)
@@ -18,7 +17,6 @@ def create_app():
     JWTManager(app)
     Migrate(app, db)
     db.init_app(app)
-
     
     api = Api(app)
     api.add_resource(ClienteResource, '/clientes', '/clientes/<int:id>')
@@ -27,9 +25,7 @@ def create_app():
     api.add_resource(ContaResource, '/conta', '/conta/<int:id>')
     api.add_resource(ClienteLogin, '/login')
     api.add_resource(LeilaoResource, '/leilao', '/leilao/<int:id>')
-    api.add_resource(LanceResource,'/lance','/lance/<int:id>')
     api.add_resource(LeilaoResourceLista, '/listaleilao')
-    api.add_resource(ProdutoResourceLista, '/listaproduto')
     
     scheduler.init_app(app)
     scheduler.start()
